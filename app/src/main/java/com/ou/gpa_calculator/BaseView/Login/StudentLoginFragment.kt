590b043850495a6d1e043b6254ea9c2e683f7412
Ayoola Masha <30448980+Ayoolamasha@users.noreply.github.com>
2021-08-29 15:37:42 +0100

    created fragment and class

---
+++ app/src/main/java/com/ou/gpa_calculator/BaseView/Login/StudentLoginFragment.kt
@@ -0,0 +1,14 @@
+package com.ou.gpa_calculator.BaseView.Login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class StudentLoginFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
