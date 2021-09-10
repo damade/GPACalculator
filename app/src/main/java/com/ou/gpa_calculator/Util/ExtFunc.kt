8610168c78235f3ddaa20312799d9ebf1e1d2539
Adeoye Damilola Tunde <damilolaadeoye545@yahoo.com>
2021-09-10 15:54:42 +0100

    Added a  model wrapper for the advice

--- app/src/main/java/com/ou/gpa_calculator/Util/ExtFunc.kt
+++ app/src/main/java/com/ou/gpa_calculator/Util/ExtFunc.kt
@@ -4,87 +4,140 @@ import com.ou.gpa_calculator.LocalData.Model.CourseInfo
 import com.ou.gpa_calculator.LocalData.Model.RawCourseData
 import com.ou.gpa_calculator.LocalData.Model.SemesterInfo
 import com.ou.gpa_calculator.LocalData.Model.SessionInfo
+import kotlinx.android.synthetic.main.fragment_advice.*
 import java.math.BigDecimal
 import java.math.RoundingMode
 import java.util.ArrayList
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
-    fun RawCourseData.toCourseInfo(): ArrayList<CourseInfo>{
-        var initData = ArrayList<CourseInfo>()
-        for (datum in data){
-            val courseInfo = CourseInfo(formDetails.year,formDetails.semester,datum.courseCode,
-                                        datum.courseTitle,datum.courseNoOfUnit,0)
-            initData.add(courseInfo)
+fun RawCourseData.toCourseInfo(): ArrayList<CourseInfo> {
+    var initData = ArrayList<CourseInfo>()
+    for (datum in data) {
+        val courseInfo = CourseInfo(
+            formDetails.year, formDetails.semester, datum.courseCode,
+            datum.courseTitle, datum.courseNoOfUnit, 0
+        )
+        initData.add(courseInfo)
+    }
+    return initData
+}
+
+fun SessionInfo.getCgpa(): Double {
+    var unitCount = 0.0
+    var gradePoint = 0.0
+
+    for (semester in semesters) {
+        unitCount += semester.totalUnits()
+        gradePoint += semester.getGP()
+    }
+
+    return (gradePoint / unitCount)
+}
+
+fun SessionInfo.getSummary(): Array<Int> {
+    var unitCount = 0
+    var gradePoint = 0
+
+    for (semester in semesters) {
+        unitCount += semester.totalUnits()
+        gradePoint += semester.getGP()
+    }
+
+    return arrayOf(gradePoint, unitCount)
+}
+
+fun SessionInfo.getAdvice(): String {
+    var advice = ""
+    var unitCount = 0
+    var gradePoint = 0
+
+    for (semester in semesters) {
+        unitCount += semester.totalUnits()
+        gradePoint += semester.getGP()
+    }
+
+    when (this.getCgpa()) {
+        in 4.5..5.0 -> advice = "You are right on track"
+        in 3.50..4.49 -> {
+            val rec = ((4.5 * ( this.getSemesterCount() + 1)) - (this.getCgpa() * this.getSemesterCount()))
+            advice = if(rec <= 5.0){
+                "You need ${rec.roundTo(2)} in your next semester to get to First Class"
+            } else{
+                "You will need steady 5.0 GPAs to be able to get to First Class"
+            }
         }
-        return initData
-    }
-
-    fun SessionInfo.getCgpa(): Double{
-        var unitCount = 0.0
-        var gradePoint = 0.0
-
-        for (semester in semesters){
-            unitCount += semester.totalUnits()
-            gradePoint += semester.getGP()
+        in 2.40..3.49 ->{
+            val rec = ((3.50 * ( this.getSemesterCount() + 1)) - (this.getCgpa() * this.getSemesterCount()))
+            advice = if(rec <= 5.0){
+                "You need ${rec.roundTo(2)} in your next semester to get to Second Class Upper"
+            } else{
+                "You will need steady 5.0 GPAs to be able to get to Second Class Upper"
+            }
         }
-
-        return (gradePoint/unitCount)
-    }
-
-    fun SessionInfo.getSummary(): Array<Int>{
-        var unitCount = 0
-        var gradePoint = 0
-
-        for (semester in semesters){
-            unitCount += semester.totalUnits()
-            gradePoint += semester.getGP()
+        in 1.50..2.39 -> {
+            val rec = ((2.40 * ( this.getSemesterCount() + 1)) - (this.getCgpa() * this.getSemesterCount()))
+            advice = if(rec <= 5.0){
+                "You need ${rec.roundTo(2)} in your next semester to get to Second Class Lower"
+            } else{
+                "You will need steady 5.0 GPAs to be able to get to Second Class Lower"
+            }
         }
-
-        return arrayOf(gradePoint,unitCount)
-    }
-
-    fun SessionInfo.getSemesterCount(): Int{
-
-        return semesters.size
-    }
-
-
-
-    fun SemesterInfo.getCourseCount(): Int{
-        return courses.size
-    }
-
-    fun SemesterInfo.totalUnits(): Int{
-        var count = 0
-        for (course in courses){
-            count += course.courseNoOfUnit
+        in 1.00..1.49 -> {
+            val rec = ((1.50 * ( this.getSemesterCount() + 1)) - (this.getCgpa() * this.getSemesterCount()))
+            advice = if(rec <= 5.0){
+                "You need ${rec.roundTo(2)} in your next semester to get to Third Class"
+            } else{
+                "You will need steady 5.0 GPAs to be able to get to Third Class"
+            }
         }
-        return count
+        else -> advice = "You are very far from any good grade, you really need to consider your options"
     }
 
-    fun SemesterInfo.getGP(): Int{
-        var gp = 0
-        for (course in courses){
-            gp += (course.courseNoOfUnit * course.grade)
-        }
-        return gp
+    return advice
+}
+
+fun SessionInfo.getSemesterCount(): Int {
+
+    return semesters.size
+}
+
+
+fun SemesterInfo.getCourseCount(): Int {
+    return courses.size
+}
+
+fun SemesterInfo.totalUnits(): Int {
+    var count = 0
+    for (course in courses) {
+        count += course.courseNoOfUnit
+    }
+    return count
+}
+
+fun SemesterInfo.getGP(): Int {
+    var gp = 0
+    for (course in courses) {
+        gp += (course.courseNoOfUnit * course.grade)
+    }
+    return gp
+}
+
+fun CourseInfo.isNotEmptyNorNull(): Boolean {
+    if (year.isNullOrEmpty() || semester.isNullOrEmpty() || courseCode.isNullOrEmpty()
+        || courseTitle.isNullOrEmpty() || courseNoOfUnit == -1 || grade == -1
+    ) {
+        return false
     }
 
-    fun CourseInfo.isNotEmptyNorNull(): Boolean{
-        if(year.isNullOrEmpty() || semester.isNullOrEmpty() || courseCode.isNullOrEmpty()
-            || courseTitle.isNullOrEmpty() || courseNoOfUnit == -1 || grade == -1){
-            return false
-        }
+    return true
+}
 
-        return true
-    }
+fun Double.roundTo(numFractionDigits: Int): Double {
+    val factor = 10.0.pow(numFractionDigits.toDouble())
+    return (this * factor).roundToInt() / factor
+}
 
-    fun Double.roundTo(numFractionDigits: Int): Double {
-        val factor = 10.0.pow(numFractionDigits.toDouble())
-        return (this * factor).roundToInt() / factor
-    }
-
-    fun <T> MutableList<T>.prepend(element: T) {
-        add(0, element)
-    }
+fun <T> MutableList<T>.prepend(element: T) {
+    add(0, element)
+}
