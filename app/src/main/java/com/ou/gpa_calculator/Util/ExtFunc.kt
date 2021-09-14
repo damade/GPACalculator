1b5807c420b2a6a1030a309172aa75bb8e073c00
Ayoola Masha <30448980+Ayoolamasha@users.noreply.github.com>
2021-09-14 05:46:11 +0100

    final touches

--- app/src/main/java/com/ou/gpa_calculator/Util/ExtFunc.kt
+++ app/src/main/java/com/ou/gpa_calculator/Util/ExtFunc.kt
@@ -62,31 +62,40 @@ fun SessionInfo.getAdvice(): String {
         in 3.50..4.49 -> {
             val rec = ((4.5 * ( this.getSemesterCount() + 1)) - (this.getCgpa() * this.getSemesterCount()))
             advice = if(rec <= 5.0){
-                "You need ${rec.roundTo(2)} in your next semester to get to First Class"
+                "You need ${rec.roundTo(2)} in your next semester to get to First Class \n " +
+                        "To achieve this you need to get more A's especially in your 3 unit courses"
             } else{
-                "You will need steady 5.0 GPAs to be able to get to First Class"
+                "You will need to make sure you are in between 4.0 to 5.0 GPAs to be able to maintain your First Class \n" +
+                        "Make sure you strive to get more A's in your 3 unit courses"
             }
         }
         in 2.40..3.49 ->{
             val rec = ((3.50 * ( this.getSemesterCount() + 1)) - (this.getCgpa() * this.getSemesterCount()))
             advice = if(rec <= 5.0){
-                "You need ${rec.roundTo(2)} in your next semester to get to Second Class Upper"
+                "You need ${rec.roundTo(2)} in your next semester to get to Second Class Upper\n " +
+                        "To achieve this you need to get more A's especially in your 3 unit courses"
             } else{
-                "You will need steady 5.0 GPAs to be able to get to Second Class Upper"
+                "You will need to make sure you are in between 3.5 to 4.0 GPAs to be able to maintain your Second Class Upper \n " +
+                        "To move up to a First Class, you need to be in between 4.0 to 5.0 \n " +
+                        "To achieve this you need to get more A's especially in your 3 unit courses and at least c in your 3 units courses"
             }
         }
         in 1.50..2.39 -> {
             val rec = ((2.40 * ( this.getSemesterCount() + 1)) - (this.getCgpa() * this.getSemesterCount()))
             advice = if(rec <= 5.0){
-                "You need ${rec.roundTo(2)} in your next semester to get to Second Class Lower"
+                "You need ${rec.roundTo(2)} in your next semester to get to Second Class Lower \n " +
+                        "To achieve this you need to get more A's especially in your 3 unit courses and at least B or C in your 2 unit courses"
             } else{
-                "You will need steady 5.0 GPAs to be able to get to Second Class Lower"
+                "To move up to Second Class Upper will need to make sure you are in between 3.5 to 4.0 GPAs to be achieve this \\n \" +\n" +
+                        "                        \"To achieve this you need to get more A's especially in your 3 unit courses \n " +
+                        "\"To move up to a First Class, you need to be in between 4.0 to 5.0 \\n \\"
             }
         }
         in 1.00..1.49 -> {
             val rec = ((1.50 * ( this.getSemesterCount() + 1)) - (this.getCgpa() * this.getSemesterCount()))
             advice = if(rec <= 5.0){
-                "You need ${rec.roundTo(2)} in your next semester to get to Third Class"
+                "You need ${rec.roundTo(2)} in your next semester to get to Third Class \n " +
+                        "To achieve this you need to get more A's especially in your 3 unit courses and at least B in your 2 unit courses"
             } else{
                 "You will need steady 5.0 GPAs to be able to get to Third Class"
             }
